68f08a41a972f8783da909ed92114a42cf3a3365
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-01-26 17:34:20 -0300

    feat: create email service

---
+++ src/main/java/br/com/gabrieudev/agenda/infrastructrure/service/EmailService.java
@@ -0,0 +1,32 @@
+package br.com.gabrieudev.agenda.infrastructrure.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import br.com.gabrieudev.agenda.application.exceptions.EmailException;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new EmailException("Erro ao enviar e-mail: " + e.getMessage());
+        }
+    }
+}
