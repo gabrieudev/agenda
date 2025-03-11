0b83fa0bc08b87954dd940c80bccb1cfc28d301d
João Gabriel <gabrieudev@gmail.com>
2025-03-11 12:09:29 -0300

    fix: add id to update notification guest dto

--- src/main/java/br/com/gabrieudev/agenda/infrastructrure/web/dtos/notificationguest/UpdateNotificationGuestDTO.java
+++ src/main/java/br/com/gabrieudev/agenda/infrastructrure/web/dtos/notificationguest/UpdateNotificationGuestDTO.java
@@ -1,9 +1,12 @@
 package br.com.gabrieudev.agenda.infrastructrure.web.dtos.notificationguest;
 
+import java.util.UUID;
+
 import org.modelmapper.ModelMapper;
 
 import br.com.gabrieudev.agenda.domain.entities.NotificationGuest;
 import br.com.gabrieudev.agenda.infrastructrure.web.dtos.status.StatusDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +16,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateNotificationGuestDTO {
+    @Schema(
+        description = "Identificador do convite",
+        example = "123e4567-e89b-12d3-a456-426614174000"
+    )
+    @NotNull(message = "Identificador obrigatório")
+    private UUID id;
+
     @NotNull(message = "Status obrigatório")
     private StatusDTO status;
 
